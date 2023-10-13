1a27667731dd44951fb80a4de6532761d40c8a88
AyushGwari <ayushgwari121@gmail.com>
2023-10-13 17:54:13 +0530

    added login functionality

--- base/views.py
+++ base/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,logout,authenticate
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
-
+@login_required(login_url='login')
 def dashboard(request):
     return render(request,'dashboard.html')
+
+def loginview(request):
+    
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            context ={'user': user}
+            return redirect(reverse('dashboard'),context)
+        
+        messages.error(request,"Invalid credentials")
+        return render(request,"login.html")
+
+    return render(request,"login.html")
--- expense_manage/urls.py
+++ expense_manage/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from base.views import dashboard
+from base.views import dashboard,loginview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/',dashboard,name = "dashboard")
+    path('dashboard/',dashboard,name = "dashboard"),
+    path('login/',loginview,name = "login")
 ]
 
 if settings.DEBUG:
