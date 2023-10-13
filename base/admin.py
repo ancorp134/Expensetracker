b8da77b3488c916d88029dc4bddc35e2927ca1be
Ankit Tyagi <ancorp007@gmail.com>
2023-10-13 16:23:53 +0530

    add model in admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employee
 
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("Emp_name","email","contract_no","contract_start_date","contract_start_date")
+
+
+admin.site.register(Employee,EmployeeAdmin)
