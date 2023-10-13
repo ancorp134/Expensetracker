7aa40e19846854c0b53827f2901b4ddd80f2dcda
AyushGwari <ayushgwari121@gmail.com>
2023-10-13 16:39:54 +0530

    added models

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+import os
 # Create your models here.
 
 class Employee(models.Model):
@@ -11,6 +12,40 @@ class Employee(models.Model):
     contract_end_date = models.DateField()
     phone_number = models.CharField(unique=True,max_length=10)
     state = models.CharField(max_length=30)
+    duty_station = models.CharField(max_length=40)
 
     def __str__(self):
         return str(self.name)
+
+
+def get_upload_path(instance,filename):
+    return os.path.join('Advance Trip Plans/' +  str(instance.employee.Emp_name),filename)
+
+class AdvancedTravelPlan(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,primary_key=True,editable=False)
+    employee = models.ForeignKey(Employee,on_delete=models.CASCADE)
+    month = models.CharField(choices = [
+        ('January', 'January'),
+        ('February', 'February'),
+        ('March' , 'March'),
+        ('April','April'),
+        ('May','May'),
+        ('June','June'),
+        ('July','July'),
+        ('August','August'),
+        ('September','September'),
+        ('October','October'),
+        ('November','November'),
+        ('December','December')
+    ],default = "January",max_length=10)
+    year = models.CharField(max_length=20,default = "2023",null=True)
+    date_added = models.DateField(auto_now_add=True)
+    trip_plan = models.FileField(upload_to=get_upload_path)
+
+    def __str__(self):
+        return str(self.employee)
+
+
+class FlightBudget(models.Model):
+    uuid = models.UUIDField(default = uuid.uuid4,primary_key=True,editable=False)
+
