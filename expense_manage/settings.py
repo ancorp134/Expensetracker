f01d6bae43c2d35927569af2e249efddd9ef80ca
Ankit Tyagi <ancorp007@gmail.com>
2023-10-17 16:32:53 +0530

    ...

--- expense_manage/settings.py
+++ expense_manage/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'expense_manage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expensedb',
+        'USER': 'root',
+        'PASSWORD':  'inductuspass',
+        'HOST': 'expensedb.c6zegveyduxa.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -120,8 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Automatically Created on Production
+# STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Automatically Created on Production
 
 
 # Settings for Media
@@ -132,3 +144,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+AWS_ACCESS_KEY_ID = 'AKIAWHJFVK777W7IMV4Q'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'inductusexpenses'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
