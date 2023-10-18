643366990cf06477681be14a1224f10b3cfec776
Ankit Tyagi <ancorp007@gmail.com>
2023-10-18 22:36:17 +0530

    ...

--- expense_manage/settings.py
+++ expense_manage/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import psycopg2
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,35 +78,17 @@ WSGI_APPLICATION = 'expense_manage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'expense',
-        'USER': 'inductus',
-        'PASSWORD': 'inductuspass',
-        'HOST': 'expense.c6zegveyduxa.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
-    } 
-} 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'induc8vs_testing',
-#         'USER': 'induc8vs_testing',
-#         'PASSWORD': 'd7gWiZFa?@u@',
-#         'HOST':'localhost',
-#         'PORT':'4000',
-#     }
-# }
+
+
+
+
 
 
 
@@ -162,15 +144,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AWS_ACCESS_KEY_ID = 'AKIAWHJFVK77YOUC6QE2'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'expense3'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
