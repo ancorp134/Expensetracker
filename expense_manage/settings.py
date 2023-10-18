7bbce2b14b85d5799dbe4f0054f266800d2a6b3e
Ankit Tyagi <ancorp007@gmail.com>
2023-10-18 21:40:43 +0530

    ....

--- expense_manage/settings.py
+++ expense_manage/settings.py
@@ -78,13 +78,24 @@ WSGI_APPLICATION = 'expense_manage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expense',
+        'USER': 'inductus',
+        'PASSWORD': 'inductuspass',
+        'HOST': 'expense.c6zegveyduxa.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    } 
+} 
 
 # DATABASES = {
 #     'default': {
